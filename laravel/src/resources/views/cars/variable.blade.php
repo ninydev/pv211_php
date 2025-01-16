edde66924f2e9cd2516609057336ca0392cbe09c
Oleksandr Nykytin <keeper@ninydev.com>
2025-01-16 19:22:10 +0200

    Add Car Slider

--- laravel/src/resources/views/cars/variable.blade.php
+++ laravel/src/resources/views/cars/variable.blade.php
@@ -6,6 +6,70 @@
     <div class="container">
         <h1>Variable of cars</h1>
 
+        <style>
+            .myColor img{
+                width: 50px;
+                height: 50px;
+                margin: 10px;
+                border-radius: 50%;
+            }
+        </style>
+
+        <section>
+
+            <div class="container mt-4">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    @foreach($cars as $car)
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link"
+                                    id="about-tab" data-bs-toggle="tab" data-bs-target="#carid_{{ $car->id  }}" type="button" role="tab"
+                                    aria-controls="about" aria-selected="true">{{ $car->name }}</button>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="tab-content" id="myTabContent">
+
+                    @foreach($cars as $car)
+                        <div class="tab-pane fade show" id="carid_{{ $car->id  }}" role="tabpanel" aria-labelledby="about-tab">
+                            <p>{{ $car->name  }}</p>
+
+                            <img id="mainImg_{{ $car->id  }}" class="w-50" />
+
+                            <div class="container">
+                                <ul class="row">
+                                    @foreach($car->colors as $carColor)
+                                        <div class="col-1 myColor">
+                                            <img src="{{ asset('storage/' . $carColor->color->url)  }}"
+                                                 data-id="mainImg_{{ $car->id  }}"
+                                                 data-url="{{ asset('storage/' . $carColor->url) }}"
+                                                 class="d-block" alt=" {{ $carColor->color->name  }}">
+                                        </div>
+                                    @endforeach
+                                </ul>
+
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
+            </div>
+
+            <script>
+                document.querySelectorAll('.myColor img').forEach(img => {
+                    img.addEventListener('click', function() {
+                        console.log(this.getAttribute('data-url'));
+                        document.getElementById(this.getAttribute('data-id')).src = this.getAttribute('data-url');
+                    });
+                });
+            </script>
+
+            <script>
+                document.getElementById('myTab').firstElementChild.classList.add('active');
+                document.getElementById('myTabContent').firstElementChild.classList.add('active');
+            </script>
+
+        </section>
+
         <section>
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner" id="carSlider1">
@@ -28,49 +92,15 @@
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+
+            <script>
+                document.getElementById('carSlider1').firstElementChild.classList.add('active');
+            </script>
         </section>
 
-        <script>
-            document.getElementById('carSlider1').firstElementChild.classList.add('active');
-        </script>
 
 
-        <main>
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
 
-            @foreach($all as $car)
-
-                        <div class="carousel-item active">
-                            <img src=" .. " class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>{{ $car->name  }}</h5>
-                                <ul>
-                                    @foreach($car->colors as $carColor)
-                                        <li>{{ $carColor->color->name }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        </div>
-
-            @endforeach
-
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-
-        </main>
 
         <div>
             <h3> Cars </h3>
