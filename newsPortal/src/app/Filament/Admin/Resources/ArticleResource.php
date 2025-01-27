b80c52e300b6f9b9e9adf6179177f5f0fceb40e6
Oleksandr Nykytin <keeper@ninydev.com>
2025-01-27 20:08:39 +0200

    Add Article Panel

---
+++ newsPortal/src/app/Filament/Admin/Resources/ArticleResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\ArticleResource\Pages;
+use App\Filament\Admin\Resources\ArticleResource\RelationManagers;
+use App\Models\Article;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ArticleResource extends Resource
+{
+    protected static ?string $model = Article::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->label('Title')
+                    ->required(),
+                Forms\Components\RichEditor::make('content')
+                    ->label('Content')
+                    ->required(),
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->label('Thumbnail'),
+                Forms\Components\TextInput::make('slug')
+                    ->label('Slug')
+                    ->required(),
+                Forms\Components\Select::make('author_id')
+                    ->label('Author')
+                    ->relationship('author', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('article_id')->label('Article ID'),
+                Tables\Columns\TextColumn::make('title')->label('Title')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('likes_count')->label('Likes Count')->counts('likes'),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticles::route('/'),
+            'create' => Pages\CreateArticle::route('/create'),
+            'edit' => Pages\EditArticle::route('/{record}/edit'),
+        ];
+    }
+}
--- newsPortal/src/app/Models/Like.php
+++ newsPortal/src/app/Models/Like.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
+    protected $fillable = [
+        'article_id',
+        'user_id',
+    ];
+
     public function article()
     {
         return $this->belongsTo(Article::class);
